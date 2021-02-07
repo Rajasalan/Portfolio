e05b238af94ad5829fe95dc1f60065a885b06455
Garland <garland@cloudronics.com>
2021-02-07 02:18:34 +0200

    initial commit for carousal,title and scroll down

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import MyNavbar from "./components/my-navbar/my-navbar.component";
+import MyCarousal from "./components/my-carousal/my-carousal.component";
+import TitleMessage from "./components/title-message/title-message.component";
 
 const App = () => {
   return (
     <div className="App">
       <MyNavbar/>
+      <MyCarousal/>
+      <TitleMessage/>
     </div>
   );
 }
